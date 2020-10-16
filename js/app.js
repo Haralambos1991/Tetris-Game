b2fdfad06151e6d0c79c222925e3624296e8b516
Your Name <tsivikaspower@gmail.com>
2020-10-16 11:45:07 -0700

    new message

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+const startBtn = document.querySelector('button')
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('.score-display')
+const linesDisplay = document.querySelectorAll('.lines-display')
 let squares = Array.from(grid.querySelectorAll('div'))
 const width = 10
 const height = 20
 let currentPosition = 4
+let timerId
+let score = 0
+let lines = 0
 
-// 
+
+
+//Assign functions to keycode
+function control(e) {
+    if(e.KeyCode === 39) {
+        moveRight()
+    } else if (e.KeyCode === 38) {
+        rotate()
+    } else if (e.KeyCode === 37) {
+      moveLeft()
+    } else if (e.KeyCode === 40) {
+        moveDown()
+    }
+    }
+    document.addEventListener('keyup', control)
+
+
+
 
 // The Tetromino
 const lTetromino = [
@@ -102,4 +125,61 @@ function rotate(){
     }
     current = theTetrominoes[random][currentRotation]
     draw()
+}
+
+
+// show previous tetromino in displaySquares
+const displayWidth = 4
+const displayIndex = 0
+const nextRandom = 0
+
+const smallTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2] /* l Tetromino */
+    [0, displayWidth, displayWidth+1, displayWidth*2+1],
+    [1, displayWidth, displayWidth+1, displayWidth+2],
+    [0, 1, displayWidth, displayWidth+1],
+    [1, displayWidth+1, displayWidth+2+1, displayWidth*3+1]
+]
+
+function displayShape() {
+    displaySquares.forEach(square => {
+        square.classList.remove('block')
+    })
+    smallTetrominoes[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('block')
+    })
+}
+
+// Right now I have to freeze the shapes
+function freeze() {
+    if(current.some(index => squares[currentPosition + Index + Width].classList.contains('block3')
+    || squares[currentPosirrentPosition].classList.add) {
+    current.forEach(index => squares[index + currentPosition].classList.add('block2'))
+
+ random = nextRandom
+ nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+ current = theTetrominoes[random][currentRotation]
+ currentPosition = 4
+ draw()
+ displayShape()
+}
+}
+
+startBtn.addEventListener('click', () => {
+    if(timerId) {
+        clearInterval(timerId)
+        timerId = null
+    } else {
+        draw()
+        timerId = setInterval(moveDown, 1000)
+        nextRandom = Math.floor(Math.random() *theTetrominoes.length)
+        displayShape()
+    }
+})
+
+//game is over 
+function gameOver() {
+    if(current.some
+        (index => squares[currentPosition]))
+
 }
